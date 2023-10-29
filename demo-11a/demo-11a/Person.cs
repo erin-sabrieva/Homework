6ef555efa4655e2c4a293f5949e3b1771a0eea3e
Erin <erin.sabrieva@gmail.com>
2023-10-29 23:01:30 +0200

    Initial Commit

---
+++ demo-11a/demo-11a/Person.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace demo_11a
+{
+	public class Person
+	{
+		private string name;
+		private int age;
+		private string gender;
+		private static string status;
+
+		public Person(string name, int age, string gender)
+		{
+			Name = name;
+			Age = age;
+			Gender = gender;
+			Status=status;
+		
+		}
+
+		public string Name
+		{
+			set { 
+				if (Regex.IsMatch(value, @"[a-zA-Z]"))
+				{ 
+					this.name = value;
+				}
+				else
+				{
+					throw new Exception("Invalid Name");
+				}
+			
+			}
+			get { return this.name; }
+			
+		}
+		public int Age
+		{
+			set
+			{
+				if (value < 0 ||value > 100)
+				{
+					throw new Exception("Invalid Age!");
+					
+				}
+				else { this.age = value; }
+			}
+			get { return this.age; }
+			
+		}
+		public string Gender
+		{
+			set
+			{
+				if (value!="female" && value != "male")
+				{
+					throw new Exception("Invalid Gender!");
+				}
+				else
+				{
+					this.gender = value;
+				}
+			}
+			get { return this.gender; }
+			
+		}
+		public static string Status 
+	    {
+			set{status= "new";}
+			get { return status;}
+	        
+        }
+		public static string Greeting()
+		{
+          return "Hi I am a person:)"; 
+		}
+		public void Eating() 
+		{
+            Console.WriteLine("I am eating breakfast");
+        }
+		public void Working()
+		{
+            Console.WriteLine("I'm doing my tasks for the day.");
+        }
+		public void Sleep()
+		{
+            Console.WriteLine("I am going to sleep");
+        }
+		
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Name: {Name} ");
+			sb.AppendLine($"Age: {Age} ");
+			sb.AppendLine($"Gender: {Gender}");
+			sb.AppendLine($"Status: {status}");
+			return sb.ToString().TrimEnd();
+		}
+	}
+}
---
+++ demo-11a/demo-11a/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace demo_11a
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Въведи брой хора");
+				int n = int.Parse(Console.ReadLine());
+				List<Person> list = new List<Person>();
+				for (int i = 0; i < n; i++)
+				{
+					Console.WriteLine("Въведи име, години и пол");
+					string[] input = Console.ReadLine().Split(" ");
+					Person person = new Person(input[0], int.Parse(input[1]), input[2].ToLower());
+					list.Add(person);
+					Console.WriteLine(Person.Greeting());
+					person.Eating();
+					person.Working();
+					person.Sleep();
+					Console.WriteLine(person.ToString());
+					Console.WriteLine();
+				}
+				Console.WriteLine($"Person Count in our program: {list.Count}");
+			}
+			catch (Exception e)
+			{
+
+                Console.WriteLine($"An error occured: {e.Message}");
+            }
+            
+
+
+        }
+	}
+}
